d1f29777cbeefa77aa5ab35d908d0b40d5f972a8
Pavithra Sivagnanasuntharam <pavithrasiv3@gmail.com>
2022-03-01 22:59:20 -0500

    Fixed issue with StateProvider / issue #16

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -2,42 +2,37 @@ import React from "react";
 import './App.css';
 import Header from './Header';
 import Home from './Home';
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Checkout from "./Checkout";
 
 
-
 function App() {
-    return ( 
+    return (
 
-    // wrapping everything with a router --> allow to have pages
-    <Router>
-        <div className = "App">
+        // wrapping everything with a router --> allow to have pages
+        <Router >
+            <div className = "App" >
+            { /* rendering the header, because it is always going to be in all the pages */ } 
+            <Header/>
 
-                {/* rendering the header, because it is always going to be in all the pages */}
-                <Header />
+            { /* THIS IS WHERE THE <SWITCH></SWITCH> SHOULD BE PLACED FOR CASES */ } { /* Switch --> Routes in the newer version of React */ } 
+            <Switch >
 
-                {/* THIS IS WHERE THE <SWITCH></SWITCH> SHOULD BE PLACED FOR CASES */}
-                {/* Switch --> Routes in the newer version of React */}
-                <Switch>
-
-                    {/* page1 --> the checkout page */}
-                    <Route path="/Checkout">
-                        <Checkout />
-                    </Route>
-                    {/* page2 --> the cart page  */}
-                    {/* page3 --> filtering page */}
-                    {/* page4 --> the promotion page */}
-                    {/* page5 --> login page  */}
-
-                    {/* pageX --> the default page, home page */}
-                    {/* this is the default route */}
-                    <Route path="/">
-                        <Home/>
-                    </Route>
-                </Switch>
-        </div>
-    </Router>
+            { /* page1 --> the checkout page */ } 
+            <Route path = "/Checkout" >
+                <Checkout/>
+            </Route> 
+            { /* page2-- > the cart page */} 
+            {/*page3-- > filtering page*/} 
+            { /* page4-- > the promotion page*/} 
+            {/*page5-- > login page */}
+            { /* pageX --> the default page, home page */ } { /* this is the default route */ }
+            <Route path = "/" >
+            <Home/>
+            </Route> 
+            </Switch > 
+            </div> 
+        </Router>
     );
 }
 
--- amazon-clone/src/StateProvider.js
+++ amazon-clone/src/StateProvider.js
@@ -4,11 +4,12 @@ import React, {createContext, useContext, useReducer} from "react";
 export const StateContext = createContext();
 
 //Wraps the app and provides the data layer
-export const StateProvider = ({reducer, initialState, children}) => {
+export const StateProvider = ({reducer, initialState, children}) => (
     <StateContext.Provider value={useReducer(reducer, initialState)}>
         {children}
     </StateContext.Provider>
-};
+);
 
 //Pull information from the data layer
 export const useStateValue = () => useContext(StateContext);
+
