bc60d76162960b1723017241e3f01629e557dec2
Pavithra Sivagnanasuntharam <pavithrasiv3@gmail.com>
2022-03-02 17:51:27 -0500

    Update on removing items from cart / issue #23

--- amazon-clone/src/CheckoutProduct.js
+++ amazon-clone/src/CheckoutProduct.js
@@ -1,7 +1,22 @@
 import React from "react";
-import "CheckoutProduct.css";
+import "./CheckoutProduct.css";
+import { useStateValue } from "./StateProvider";
 
 function CheckoutProduct({id, image, title, price, rating}) {
+    
+    const [{basket}, dispath] = useStateValue();
+
+
+    const removeFromBasket = () => {
+        // this is going to be remove the item fro the basket 
+        dispatchEvent({
+            type : 'REMOVE_FROM_BASKET', 
+            id: id,
+        })
+
+    }
+    
+    
     return (
         <div className="checkoutProduct">
 
@@ -11,6 +26,16 @@ function CheckoutProduct({id, image, title, price, rating}) {
             {/* contains every other information of the product - title, price, rating, 'Remove' button */}
             <div className="checkoutProduct__info">
                 <p className="checkoutProduct__title">{title}</p>
+                <p className="checkoutProduct__price"><small>$</small><strong>{price}</strong></p>
+
+                <div className="checkoutProduct__rating">
+                    {Array(rating)
+                    .fill()
+                    .map((_,i) => (
+                        <p>*</p>
+                    ))}
+                </div>
+                <button onClick = {removeFromBasket}>Remove from Cart</button>
             </div>
         </div>
     )
--- amazon-clone/src/reducer.js
+++ amazon-clone/src/reducer.js
@@ -19,6 +19,31 @@ const reducer = (state, action) => {
                 // push the item into the basket
                 basket: [...state.basket, action.item], //Current state of basket, the action that is going to be done
             };
+        case "REMOVE_FROM_BASKET":
+            
+            // we want to make sure that all of the items from same id doesnt get removed
+            //remove the first match
+            const index = state.basket.findIndex(
+                (basketItem) => basketItem.id === action.id
+            );
+
+             //this is the new basket
+             let newBasket = [...state.basket];
+
+            if (index >= 0){
+                //find item in the basket
+                //this is going to be creating a new basket
+                newBasket.splice(index, 1);
+            } else {
+                console.warn(
+                    `Cant remove product(id: ${action.id}) as its not in basket!`
+                )
+            }
+            return {
+                ...state,
+                basket: newBasket
+            }
+        
         default:
             return state;
     }
