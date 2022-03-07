e187207e30d7c7ac99c0d6a1412e721c7c7412ad
asha97 <85359230+asha97@users.noreply.github.com>
2022-03-07 01:28:25 -0500

    Setting the footer into the home page + styling of the footer / issue #31

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -6,8 +6,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Checkout from "./Checkout";
 import NavigationBar from "./NavigationBar";
 import ProductDetails from "./ProductDetails";
-import Footer from './Footer';
-
+import Footer from './Footer'
 
 function App() {
     return (
@@ -15,42 +14,41 @@ function App() {
         // wrapping everything with a router --> allow to have pages
         <Router >
             <div className = "App" >
-            { /* rendering the header, because it is always going to be in all the pages */ } 
-            <Header/>
-            <NavigationBar/>
+                    { /* rendering the header, because it is always going to be in all the pages */ } 
+                    <Header/>
+                    <NavigationBar/>
+                    {/* This is where we will have the banner, which will contain a few navigation fields */}
+                
+                    { /* THIS IS WHERE THE <SWITCH></SWITCH> SHOULD BE PLACED FOR CASES */ } { /* Switch --> Routes in the newer version of React */ } 
+                    <Switch >
 
-            {/* This is where we will have the banner, which will contain a few navigation fields */}
-        
-            { /* THIS IS WHERE THE <SWITCH></SWITCH> SHOULD BE PLACED FOR CASES */ } { /* Switch --> Routes in the newer version of React */ } 
-            <Switch >
-
-            { /* page1 --> the checkout page */ } 
-            <Route path = "/Checkout" >
-                <Checkout/>
-            </Route> 
-
-
-            <Route path = "/filtering-product" >
-                <filtering-product />
-            </Route>
+                    { /* page1 --> the checkout page */ } 
+                    <Route path = "/Checkout" >
+                        <Checkout/>
+                    </Route> 
 
 
-            { /* page2-- > the cart page */} 
-            {/*page3-- > filtering page*/} 
-            { /* page4-- > the promotion page*/} 
-            {/*page5-- > login page */}
-            {/* page6-- > product detail page */}
-            <Route path = "/ProductDetails">
-                <ProductDetails/>
-            </Route>
-            { /* pageX --> the default page, home page */ } { /* this is the default route */ }
-            <Route path = "/" >
-            <Home/>
-            </Route> 
-            </Switch > 
+                    <Route path = "/filtering-product" >
+                        <filtering-product />
+                    </Route>
 
 
-            <Footer /> 
+                    { /* page2-- > the cart page */} 
+                    {/*page3-- > filtering page*/} 
+                    { /* page4-- > the promotion page*/} 
+                    {/*page5-- > login page */}
+                    {/* page6-- > product detail page */}
+                    <Route path = "/ProductDetails">
+                        <ProductDetails/>
+                    </Route>
+                    { /* pageX --> the default page, home page */ } { /* this is the default route */ }
+                    <Route path = "/" >
+                    <Home/>
+                    </Route> 
+                    </Switch > 
+
+                    <Footer />
+
 
             </div> 
 
--- amazon-clone/src/Footer.js
+++ amazon-clone/src/Footer.js
@@ -1,13 +1,57 @@
-import React from 'react'
-import "/Footer.css";
+// need to create pages called 
+// about-us.js
+// team.js
+// contact-us.js
+
+
+import React from 'react';
+import './Footer.css';
 
 function Footer() {
   return (
-    <div className = "footer">
+    <section className='footer'>
+        <br />
+        <hr className='footer-separator' />
+        <br />
+        <br />
         
+        {/* this is the top portion of the footer */}
+        {/* <section className='footer-up'>
+           
+            <a href ="/" target="_blank" rel = "noopener noreferrer">GitHub Link</a>
+        
+        </section> */}
 
-    </div>
+        {/* this is the bottom portion of the footer */}
+        <section className='footer-bottom'>
+
+            <section className='footer-left'>
+                <section>
+                    <a href = '/About-us'>About Us </a>
+                </section>
+            </section>
+
+            <section className='footer-middle'>
+                <section>
+                    <a href = '/Team-Members'>Team Members </a>
+                </section>
+            </section>
+
+            <section className='footer-right'>
+                <section>
+                    <a href = '/Contact'>Contact Us! </a>
+                </section>
+                <br />
+
+            </section>
+
+
+
+        </section>
+        <hr className='footer-separator' />
+    </section>
+
   )
 }
 
-export default Footer
+export default Footer;
