358bd46a0f896614d18b20a98aa5bc0dde9ae9c2
asha97 <85359230+asha97@users.noreply.github.com>
2022-03-14 00:22:42 -0400

    changing css of the promo page / issue #12

--- amazon-clone/src/Promotion.js
+++ amazon-clone/src/Promotion.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react';
+import Product from "./Product";
 import "./Promotion.css";
 
 
@@ -18,6 +19,30 @@ function Promotion() {
 
         <div id="pageTitle" >
             <p>Deals of the Day</p>
+
+
+            <div className='promo_container'>
+                <div className='promo_row'>
+                <Product id = "1234" title = "Gucci Belt" price ={290.00} rating = {5} image = "https://media.gucci.com/style/DarkGray_Center_0_0_490x490/1580211003/406831_0YA0G_1000_001_100_0000_Light.jpg" />
+                <Product id = "1234" title = "Gucci Belt" price ={290.00} rating = {5} image = "https://media.gucci.com/style/DarkGray_Center_0_0_490x490/1580211003/406831_0YA0G_1000_001_100_0000_Light.jpg" />
+                <Product id = "1234" title = "Gucci Belt" price ={290.00} rating = {5} image = "https://media.gucci.com/style/DarkGray_Center_0_0_490x490/1580211003/406831_0YA0G_1000_001_100_0000_Light.jpg" />
+
+
+                </div>
+
+
+                <div className='promo_row'>
+
+                </div>
+
+            </div>
+
+
+
+
+
+
+
         </div>
 
 
@@ -27,6 +52,7 @@ function Promotion() {
         <br />
 
     </div>
+    
   )
 }
 
