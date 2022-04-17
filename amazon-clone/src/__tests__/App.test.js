37395fefa8dbdb1a86986a3f5e9718b66d20ad83
programer653 <aminata.lo@live.com>
2022-04-17 14:17:02 -0400

    added the static analysis tool

--- amazon-clone/src/__tests__/App.test.js
+++ amazon-clone/src/__tests__/App.test.js
@@ -1,21 +1,13 @@
-// import { render, screen } from '@testing-library/react';
-// import App from '../App';
-// import React from 'react';
-
-  
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+import React from 'react';
 
 
-// // test('renders App.js', () => {
-// //             render( < App /> );           
-            
-// //             const linkElement = screen.getByText('Post a New Question!');
-// //             expect(linkElement).toBeInTheDocument();
-// //           });
 
-     
 
-// test('renders learn react link', () => {
-//   const { getByText } = render(<App />);
-//   const linkElement = getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+test('renders App.js', () => {
+    render( < App / > );
+
+    const linkElement = screen.getByText('Post a New Question!');
+    expect(linkElement).toBeInTheDocument();
+});
